022f578eb139941fa5f0f427f5a73c59e4aa5b00
elFua <info>
2017-10-24 00:18:23 +0200

    - fix M3U playlist format - build tools

--- build-tools/setup-cx-freeze-mac.py
+++ build-tools/setup-cx-freeze-mac.py
@@ -4,7 +4,7 @@ from cx_Freeze import setup, Executable
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {"packages": ["os"],
     "excludes": ['gtk', 'PyQt4', 'PyQt5', 'Tkinter', 'email', 'distutils', 'logging', 'unittest'],
-    "include_files":  ['..\\lib\\']}
+    "include_files":  ['../lib/']}
 
 
 # GUI applications require a different base on Windows (the default is for a
@@ -16,9 +16,9 @@ base = None
 setup(name="Media Browser",
       version="0.8088",
       description="Local file system media browser running on http://localhost:8088",
-      icon="..\\lib\\ico\\favicon.ico",
+      icon="../lib/ico/favicon.ico",
       options={"build_exe": build_exe_options},
-      executables=[Executable("..\mediaserver.py",
+      executables=[Executable("../mediaserver.py",
                               shortcutName="MediaBrowser",
                               shortcutDir="DesktopFolder",
                               base=base)])
